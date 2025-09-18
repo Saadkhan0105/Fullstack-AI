94b8df8d343b0b42fe1df594d76f0e335b4ca06d
Saadkhan0105 <saadkhan9560@gmail.com>
2025-09-18 21:26:36 +0530

    his is a non-python file for testing purposes.

---
+++ 00_python/non_pythin_shop.py
@@ -0,0 +1,14 @@
+class Chai:
+    def __init__(self, sweetness, milk_level):
+        self.sweetness = sweetness
+        self.milk_level = milk_level
+        
+    def sip(self):
+        print("Sipping the chai...")
+        
+    def add_sugar(self, amount):
+        print("Adding sugar...")
+        
+my_chai = Chai(sweetness=5, milk_level=3)
+
+my_chai.add_sugar(2)
